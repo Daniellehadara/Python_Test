caabad6bcc06c2425d168b9e9e7d9ccb236d3b66
Daniellehadara <danielle.cohen@charite.de>
2020-03-09 10:36:15 +0100

    test

--- Test_py.py
+++ Test_py.py
@@ -3,4 +3,7 @@ import pandas as pd
 Data= {'First':('A', 'B')}
 df= pd.DataFrame(Data, columns=['First'])
 print(df)
+<<<<<<< Updated upstream
 print ("Hello shalom ve boker tov")
+=======
+>>>>>>> Stashed changes
